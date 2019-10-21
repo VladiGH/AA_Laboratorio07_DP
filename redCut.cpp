8cad02f5e6d219bad8e02f582fa11febf722d17b
Andre <00103017@uca.edu.sv>
2019-10-21 15:43:35 -0600

    fist commit

---
+++ redCut.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+/*
+    Se tiene una cuerda de 𝑛 pulgadas y una tabla con los precios 𝑝𝑖,𝑖=1,2,...,𝑛 según 
+    la longitud. Determinar la venta máxima𝑟𝑛que se puede obtener al cortar la cuerda 
+    y vender los trozos.Si el precio 𝑝𝑛es suficientemente elevado, no es necesario cortar la cuerda.
+*/
+
+
+int main(){
+
+
+
+
+    return 0;
+}
